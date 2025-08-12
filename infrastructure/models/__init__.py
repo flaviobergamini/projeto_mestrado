f497f0cbe364692767544f66df00a081f6ea68af
flaviobergamini <flaviohenrique@gec.inatel.br>
2025-08-11 22:55:25 -0300

    feat: creating ORM classes for modeling database tables

---
+++ infrastructure/models/__init__.py
@@ -0,0 +1,39 @@
+from .conversation import Conversation
+from .message import Message
+from .diary_embedding import DiaryEmbedding
+from .users import User
+from .audit import Audit
+from .autismia import Autismia
+from .beneficiary_clinic import BeneficiaryClinic
+from .beneficiary import Beneficiary
+from .clinic import Clinic
+from .evaluation import Evaluation
+from .family_reunion import FamilyReunion
+from .health_plan import HealthPlan
+from .professional import Professional
+from .school_feedback import SchoolFeedback
+from .school import School
+from .supervisor import Supervisor
+from .therapeutic_plan import TherapeuticPlan
+from .therapeutic_sessions import TherapeuticSessions
+
+__all__ = [
+    "User",
+    "Conversation",
+    "Message",
+    "DiaryEmbedding",
+    "Audit",
+    "Autismia",
+    "BeneficiaryClinic",
+    "Beneficiary",
+    "Clinic",
+    "Evaluation",
+    "FamilyReunion",
+    "HealthPlan",
+    "Professional",
+    "SchoolFeedback",
+    "School",
+    "Supervisor",
+    "TherapeuticPlan", 
+    "TherapeuticSessions",
+]
