df3f4056d668c961dda1efc27e85edb60c76cf9c
flaviobergamini <flaviohenrique@gec.inatel.br>
2025-09-01 19:55:29 -0300

    feat: creating use case to register user, jwt service and add not found property on Result

--- core/kernel/result.py
+++ core/kernel/result.py
@@ -9,14 +9,24 @@ E = TypeVar("E")
 class Result(Generic[T, E]):
     value: Optional[T] = None
     error: Optional[E] = None
+    bad_request_error: Optional[E] = None
+    not_found_error: Optional[E] = None
 
     @property
     def is_ok(self) -> bool:
-        return self.error is None
+        return self.error is None and self.bad_request_error is None and self.not_found_error is None
 
     @property
     def is_err(self) -> bool:
-        return self.error is not None
+        return self.error is not None or self.bad_request_error is not None or self.not_found_error is not None
+    
+    @property
+    def is_bad_request(self) -> bool:
+        return self.bad_request_error is not None
+    
+    @property
+    def is_not_found(self) -> bool:
+        return self.not_found_error is not None
 
     @staticmethod
     def ok(value: T) -> "Result[T, E]":
@@ -25,3 +35,11 @@ class Result(Generic[T, E]):
     @staticmethod
     def err(error: E) -> "Result[T, E]":
         return Result(error=error)
+    
+    @staticmethod
+    def bad_request(error: E) -> "Result[T, E]":
+        return Result(bad_request_error=error)
+    
+    @staticmethod
+    def not_found(error: E) -> "Result[T, E]":
+        return Result(not_found_error=error)
