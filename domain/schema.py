7ad0cec3828ef548409f79a8dcd353ab06f61b79
flaviobergamini <flaviohenrique@gec.inatel.br>
2025-09-01 19:52:41 -0300

    feat: creating model classes

--- domain/schema.py
+++ domain/schema.py
@@ -1,7 +1,23 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 from typing import List
 
 
 class RAGRequest(BaseModel):
     pergunta: str
     model: str
+
+class UserRegister(BaseModel):
+    name:str
+    email: EmailStr
+    password: str
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
+class ForgotPassword(BaseModel):
+    email: EmailStr
+
+class ResetPassword(BaseModel):
+    token: str
+    new_password: str
