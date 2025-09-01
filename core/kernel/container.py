d4ad1b8c7079759705234ee77a8f869c9b1fca5e
flaviobergamini <flaviohenrique@gec.inatel.br>
2025-08-31 21:49:59 -0300

    feat: diary embedding groq repository class, llm_service class with langchain and registering on container

--- core/kernel/container.py
+++ core/kernel/container.py
@@ -4,10 +4,12 @@ from dependency_injector.wiring import Provide, inject
 from core.use_case.diary_embedding_use_case import DiaryEmbeddingUseCase
 from core.use_case.query_diary_use_case import QueryDiaryUseCase
 from infrastructure.database_context.database import Database
+from infrastructure.repositories.diary_embedding_groq_repository import DiaryEmbeddingGroqRepository
 from infrastructure.repositories.diary_embedding_repository import DiaryEmbeddingRepository
 from infrastructure.services.deepseek_service import DeepseekService
 from infrastructure.services.gemini_service import GeminiService
 from infrastructure.services.gpt_service import GptService
+from infrastructure.services.llm_service import LLMService
 
 
 class Container(containers.DeclarativeContainer):
@@ -23,23 +25,30 @@ class Container(containers.DeclarativeContainer):
     gpt_service = providers.Factory(GptService)
     gemini_service = providers.Factory(GeminiService)
     deepseek_service = providers.Factory(DeepseekService)
+    llm_service = providers.Factory(LLMService)
 
     # Repositories
     diary_embedding_repository=providers.Factory(
         DiaryEmbeddingRepository, database=database
     )
 
+    diary_embedding_groq_repository=providers.Factory(
+        DiaryEmbeddingGroqRepository, database=database
+    )
+
     # Use cases
     diary_embedding_use_case=providers.Factory(
         DiaryEmbeddingUseCase,
         diary_embedding_repository=diary_embedding_repository,
-        gpt_service=gpt_service
+        llm_service=llm_service,
+        diary_embedding_groq_repository=diary_embedding_groq_repository
     )
 
     query_diary_use_case=providers.Factory(
         QueryDiaryUseCase,
         diary_embedding_repository=diary_embedding_repository,
-        gpt_service=gpt_service
+        llm_service=llm_service,
+        diary_embedding_groq_repository=diary_embedding_groq_repository
     )
 
 
---
+++ infrastructure/services/llm_service.py
@@ -0,0 +1,47 @@
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_groq import ChatGroq
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from sentence_transformers import SentenceTransformer
+from core.config import settings
+import asyncio
+
+class LLMService:
+    def __init__(self):
+        pass
+
+    def configure(self, provider: str):
+        self.provider = provider
+
+        if self.provider == "openai":
+            self.llm = ChatOpenAI(model=settings.GPT_MODEL, api_key=settings.OPENAI_API_KEY)
+            self.embedding_model = OpenAIEmbeddings(model=settings.GPT_EMBEDDING_MODEL, api_key=settings.OPENAI_API_KEY)
+
+        elif self.provider == "groq":
+            self.llm = ChatGroq(model=settings.GROQ_MODEL, api_key=settings.GROQ_API_KEY)
+            # para embeddings você pode usar HuggingFace local
+            self.embedding_model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
+
+        else:
+            raise ValueError("Provider não suportado")
+
+    async def chat(self, prompt: str) -> str:
+        resp = self.llm.invoke(prompt)
+        return resp.content
+
+    async def generate_embeddings(self, text: str) -> list[float]:
+        if self.provider == "groq":
+            # HuggingFace já roda local
+            return await asyncio.to_thread(self.embedding_model.encode, text)
+        else:
+            return self.embedding_model.embed_query(text)
+
+    def split_text(self, text: str) -> list[str]:
+        splitter = RecursiveCharacterTextSplitter(
+            chunk_size=500,
+            chunk_overlap=50,
+            separators=["\n\n", "\n", ".", " ", ""]
+        )
+        return splitter.split_text(text)
+    
+    def getProvider(self) -> str:
+        return self.provider
