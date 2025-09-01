15c075a72754ab40698c57dd6ec3e727b451c7df
flaviobergamini <flaviohenrique@gec.inatel.br>
2025-09-01 20:38:34 -0300

    feat: adding unauthorized property on result

--- core/kernel/result.py
+++ core/kernel/result.py
@@ -11,14 +11,15 @@ class Result(Generic[T, E]):
     error: Optional[E] = None
     bad_request_error: Optional[E] = None
     not_found_error: Optional[E] = None
+    unauthorized_error: Optional[E] = None
 
     @property
     def is_ok(self) -> bool:
-        return self.error is None and self.bad_request_error is None and self.not_found_error is None
+        return self.error is None and self.bad_request_error is None and self.not_found_error is None and self.unauthorized_error is None
 
     @property
     def is_err(self) -> bool:
-        return self.error is not None or self.bad_request_error is not None or self.not_found_error is not None
+        return self.error is not None or self.bad_request_error is not None or self.not_found_error is not None and self.unauthorized_error is not None
     
     @property
     def is_bad_request(self) -> bool:
@@ -27,6 +28,10 @@ class Result(Generic[T, E]):
     @property
     def is_not_found(self) -> bool:
         return self.not_found_error is not None
+    
+    @property
+    def is_unauthorized(self) -> bool:
+        return self.unauthorized_error is not None
 
     @staticmethod
     def ok(value: T) -> "Result[T, E]":
@@ -43,3 +48,7 @@ class Result(Generic[T, E]):
     @staticmethod
     def not_found(error: E) -> "Result[T, E]":
         return Result(not_found_error=error)
+    
+    @staticmethod
+    def unauthorized(error: E) -> "Result[T, E]":
+        return Result(unauthorized_error=error)
