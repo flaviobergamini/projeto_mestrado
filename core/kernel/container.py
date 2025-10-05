15472dc74aa6e9732382fc4df8b4a2bc9f342807
flaviobergamini <flaviohenrique@gec.inatel.br>
2025-10-04 22:13:31 -0300

    feat: delete gemini_service and your import in container

--- core/kernel/container.py
+++ core/kernel/container.py
@@ -27,7 +27,6 @@ from infrastructure.repositories.health_plan_repository import HealthPlanReposit
 from infrastructure.repositories.school_repository import SchoolRepository
 from infrastructure.repositories.user_repository import UserRepository
 from infrastructure.services.deepseek_service import DeepseekService
-from infrastructure.services.gemini_service import GeminiService
 from infrastructure.services.gpt_service import GptService
 from infrastructure.services.llm_service import LLMService
 
@@ -43,7 +42,6 @@ class Container(containers.DeclarativeContainer):
 
     # Services
     gpt_service = providers.Factory(GptService)
-    gemini_service = providers.Factory(GeminiService)
     deepseek_service = providers.Factory(DeepseekService)
     llm_service = providers.Factory(LLMService)
 
